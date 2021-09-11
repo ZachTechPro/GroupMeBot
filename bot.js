a319296691b748454683a5c974f6f8939321b389
Brett Madden <zbmadden18@gmail.com>
2021-09-11 12:22:52 -0400

    getting ready to test new commans

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
+var test = require('funny-commands');
 
 var botID = process.env.BOT_ID;
 
@@ -8,7 +9,23 @@ function respond() {
   //Here is the request
   var request = JSON.parse(this.req.chunks[0])
   var coolGuyRegex = /^\/cool guy$/;
+  var testRegex =  /^\/test$/;
 
+}
+
+function handleTest(request, testRegex){
+  if(request.text && testRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postTestMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+}
+
+function handleCoolGuy(request, coolGuyRegex){
   if(request.text && coolGuyRegex.test(request.text)) {
     this.res.writeHead(200);
     postCoolGuyMessage();
@@ -18,6 +35,41 @@ function respond() {
     this.res.writeHead(200);
     this.res.end();
   }
+}
+
+function postTestMessage(){
+  var botResponse, options, body, botReq;
+
+  botResponse = test();
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+    if(res.statusCode == 202) {
+      //neat
+    } else {
+      console.log('rejecting bad status code ' + res.statusCode);
+    }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
 }
 
 function postCoolGuyMessage() {
